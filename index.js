c018b974bddd10a44d05e84bcf6288e243cf9d09
Shustrila <sanya.maxalov@gmail.com>
2019-10-01 13:59:00 +0300

    2 files modified

--- index.js
+++ index.js
@@ -1,20 +1,15 @@
 import React, { useEffect } from 'react'
 import { render } from 'react-dom'
 import { observer } from "mobx-react"
-import * as firebase from 'firebase'
 
 import Header from './components/Header'
 import Chat from './components/Chat'
-
-import { firebaseConfig } from './assets/config'
 
 import { getMessages } from './firebase/messages'
 
 import state from './state'
 
 import './index.css'
-
-// firebase.initializeApp(firebaseConfig);
 
 const App = () => {
   const { messages } = state
